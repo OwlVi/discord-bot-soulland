d85ed764a9fb431a745e25a6ec8037d4d00b2aa6
OwlVi <mrowlvi@gmail.com>
2025-06-10 13:45:52 +0700

    Add Use Role to run command

--- config.py
+++ config.py
@@ -5,5 +5,7 @@ load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
 CHANNEL_ID = int(os.getenv("CHANNEL_ID", 0))
-if not TOKEN or CHANNEL_ID == 0:
-    raise ValueError("Please set DISCORD_TOKEN and CHANNEL_ID in your .env file")
+ROLE_ID = int(os.getenv("RESTART_ROLE_ID", 0))
+
+if not TOKEN or CHANNEL_ID == 0 or ROLE_ID == 0:
+    raise ValueError("Please set TOKEN, CHANNEL_ID, and RESTART_ROLE_ID in your .env file")
